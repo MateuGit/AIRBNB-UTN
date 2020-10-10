8fe643945496eee011f32360fe24e585b4c0e2a7
MateuGit <facundomateu@hotmail.com>
2020-10-10 18:46:33 -0300

    [ADD] client attributes in Reservations

--- airbnb/Reservations/models.py
+++ airbnb/Reservations/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class Reservation(models.Model):
     creationDate = models.DateField(auto_now=True, auto_now_add=False)
-    code = Reservation.objects.latest(); 
-    #datos de la reserva nombre, apellido y email
+    code = Reservation.objects.latest() 
     totalPrice =  models.FloatField()
+    clientName = models.TextField()
+    clientLastName = models.TextField()
+    clientEmail = models.EmailField(max_length = 80)
