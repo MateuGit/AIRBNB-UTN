30f4c0bb6ddd807af543f253e839f9da33c2f2e5
EnzoRoselli <enzoroselli1999@gmail.com>
2020-10-22 18:33:03 -0300

    [Enhance] agregando campos a las vistas

--- airbnb/ownerships/urls.py
+++ airbnb/ownerships/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
-    path('grid/', views.grid, name='grid'),
-    path('reserve/', views.reserve, name='grid'),
-    path('ownerships/', views.landing, name='ownerships'),
-    path('ownerships/<int:ownership_id>/', views.detail, name='ownership-detail'),
+    path('propiedades/', views.grid, name='ownerships'),
+    path('propiedades/<int:ownership_id>/', views.reserve, name='reserve'),
 ]
--- airbnb/ownerships/views.py
+++ airbnb/ownerships/views.py
@@ -14,9 +14,10 @@ def grid(request):
     ownerships = Ownership.objects.all()
     return render(request, 'ownership/grid.html', {'ownerships':ownerships})
 
-def reserve(request):
-    ownerships = Ownership.objects.all()
-    return render(request, 'ownership/reserve.html', {'ownerships':ownerships})
+def reserve(request, ownership_id):
+    ownership = get_object_or_404(Ownership, pk=ownership_id)
+    commission = ownership.dailyRate * 0.08
+    return render(request, 'ownership/reserve.html', {'ownership':ownership, "commission":commission})
 
 def detail(request, ownership_id):
     ownership = get_object_or_404(Ownership, pk=ownership_id)
